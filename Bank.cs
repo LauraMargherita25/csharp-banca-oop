f1c3820723729496980743f1f759b5b44c5b8e9c
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-07-04 17:02:25 +0200

    Init class Bank

---
+++ Bank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_banca_oop
+{
+    internal class Bank
+    {
+        public string Name { get; set; }
+        private List<Customer> customers;
+        private List<Loan> loans = new List<Loan>();
+
+        public Bank (string name)
+        {
+            this.Name = name;
+            this.customers = new List<Customer>();
+            this.loans = new List<Loan>();
+        }
+    }
+}
