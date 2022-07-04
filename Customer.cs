604b28027e1d73e43cc0d0655d475d86702110cf
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-07-04 17:04:24 +0200

    Init class Customer

---
+++ Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace csharp_banca_oop
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string TaxCode { get; set; }
+        public int Salary { get; set; }
+
+        public Customer (string name, string surname, string taxCode, int salary)
+        {
+            Name = name;
+            Surname = surname;
+            TaxCode = taxCode;
+            Salary = salary;
+        }
+    }
+}
